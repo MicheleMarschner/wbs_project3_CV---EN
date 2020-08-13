a03ddb8097633a8e9c7ee28569fafd807af040a0
Michele Marschner <michele.marschner@gmail.com>
2020-08-13 22:06:35 +0200

    translated CV to English - some sections still missing

---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+
+
+/*
+    let progressbar = $('progress'),
+        time = (1000/100)*5, 
+        value = 0
+        maxValue = progressbar.val();
+   
+    let loading = function() {
+        value += 1;
+        addValue = progressbar.val(value);  
+        
+        $('.progress-value').html(value + '%');
+   
+       if (value ==  maxValue) {
+        clearInterval(animate);    
+        }
+       
+    };
+   
+    let animate = setInterval(function() {
+        loading();
+    }, time);*/
+
+});
